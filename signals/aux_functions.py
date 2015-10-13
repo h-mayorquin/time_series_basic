b517075ae2614ad78a25c425a02503407374db92
heberto <h.mayorquin@gmail.com>
2015-10-13 17:31:48 +0200

    Made a gaussian bump function

--- signals/aux_functions.py
+++ signals/aux_functions.py
@@ -42,3 +42,45 @@ def bump(t, offset, center, Max):
     y[ y < 0] = 0
 
     return y
+
+
+def gaussian_bump(x, mean=0, maximum=1.0,  baseline=0, HWHM_a=1.0, attenuation=2):
+    """
+    
+    Returns a gaussian bump (something of the shape exp(-x^2)) with
+    a given maximum and a given baseline.
+
+    Parameters
+    -----------
+    x : the values of the argument
+
+    mean : the value at which the function is centered
+
+    maximum : the maximum value that the function attains at the mean
+
+    baseline : the value far from the mean, the baseline (zero shited)
+
+    HWHM_a : This come from the half width at half maximum terminology.
+    In this case it denotes the value of the argument at which the
+    function will have attenuated by the attenuation value (next arg)
+
+    attenuation value: the attenuation value of the HWHM_a.
+
+    In brief for the last two arguments:
+
+    gaussian_bumps(HWHM_a) = maximum / attenuation
+    
+    """
+
+    # First we will calculate sigma based on all the other values
+    arg1 = attenuation * (maximum - baseline)
+    arg2 = (maximum- attenuation * baseline)
+    A = np.log(arg1 / arg2)
+
+    sigma = (HWHM_a - mean) / np.sqrt(A)
+
+    argument = (x - mean) / sigma
+    gaussian = np.exp(-argument ** 2)
+    
+    return (maximum - baseline) * gaussian + baseline
+
