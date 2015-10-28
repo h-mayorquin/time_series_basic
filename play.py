d7de60148023881f104708427fd953e0f1ba5f9b
heberto <h.mayorquin@gmail.com>
2015-10-28 16:22:51 +0100

    The parameter search is working for a smaller subest, furtehrmore the visualizatio nof those pre-generated patterns is selected as well

--- play.py
+++ play.py
@@ -19,174 +19,64 @@ base = 10
 distance = 300
 value = 50
 
-distances = np.linspace(0, 600, 100)
-bases = np.linspace(0, 200, 500)
-values = np.linspace(0, 200, 50)
+distances = np.linspace(0, 600, 601)
+bases = np.linspace(0, 200, 201)
+values = np.linspace(10, 200, 191)
 
 swep_distances = True
 swep_bases = True
 swep_values = True
+verbose = True
 
 ########
 # Swep distances
 ########
 
-if swep_distances:
+for base in bases:
     for distance in distances:
-        # SLE
-        fig = parameter_swep_SLM(base, distance, value)
-        # Get the filename righ
-        name = 'parameter_swep'
-        name += '_SLM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
+        for value in values:
+            if verbose:
+                print(base, distance, value)
+            # SLE
+            fig = parameter_swep_SLM(base, distance, value)
+            # Get the filename righ
+            name = 'parameter_swep'
+            name += '_SLM'
+            filename = create_filename(directory, name, figure_format, base, distance, value)
+            # Save the figure
+            plt.savefig(filename)
+            # Clear the figure
+            plt.close(fig)
 
-        # STDM
-        fig = parameter_swep_STDM(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_STDM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
+            # STDM
+            fig = parameter_swep_STDM(base, distance, value)
+            # Get the filename right
+            name = 'parameter_swep'
+            name += '_STDM'
+            filename = create_filename(directory, name, figure_format, base, distance, value)
+            # Save the figure
+            plt.savefig(filename)
+            # Clear the figure
+            plt.close(fig)
 
-        # Cluster
-        fig = parameter_swep_cluster(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_cluster'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
+            # Cluster
+            fig = parameter_swep_cluster(base, distance, value)
+            # Get the filename right
+            name = 'parameter_swep'
+            name += '_cluster'
+            filename = create_filename(directory, name, figure_format, base, distance, value)
+            # Save the figure
+            plt.savefig(filename)
+            # Clear the figure
+            plt.close(fig)
 
-        # Cluster-SLE
-        fig = parameter_swep_cluster_SLM(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_cluster_SLM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-########
-# Swep bases
-########
-        
-if swep_bases:
-    for base in bases:
-        # SLE
-        fig = parameter_swep_SLM(base, distance, value)
-        # Get the filename righ
-        name = 'parameter_swep'
-        name += '_SLM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-        # STDM
-        fig = parameter_swep_STDM(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_STDM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-        # Cluster
-        fig = parameter_swep_cluster(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_cluster'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-        # Cluster-SLE
-        fig = parameter_swep_cluster_SLM(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_cluster_SLM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-########
-# Swep values
-########
-if swep_values:        
-    for value in values:
-        # SLE
-        fig = parameter_swep_SLM(base, distance, value)
-        # Get the filename righ
-        name = 'parameter_swep'
-        name += '_SLM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-        # STDM
-        fig = parameter_swep_STDM(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_STDM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-        # Cluster
-        fig = parameter_swep_cluster(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_cluster'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-        # Cluster-SLE
-        fig = parameter_swep_cluster_SLM(base, distance, value)
-        # Get the filename right
-        name = 'parameter_swep'
-        name += '_cluster_SLM'
-        filename = create_filename(directory, name, figure_format, base, distance, value)
-        # Save the figure
-        plt.savefig(filename)
-        print(filename)
-        # Clear the figure
-        plt.close(fig)
-
-
-
+            # Cluster-SLE
+            fig = parameter_swep_cluster_SLM(base, distance, value)
+            # Get the filename right
+            name = 'parameter_swep'
+            name += '_cluster_SLM'
+            filename = create_filename(directory, name, figure_format, base, distance, value)
+            # Save the figure
+            plt.savefig(filename)
+            # Clear the figure
+            plt.close(fig)
