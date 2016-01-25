2adb930a965f56f8a646ed86da7db52b5baf6407
heberto <h.mayorquin@gmail.com>
2016-01-25 18:27:37 +0100

    Added new presentation for the spatio temporal clusters, and modified the saving file, removed the test for storage as well

--- nexa/saving.py
+++ nexa/saving.py
@@ -104,7 +104,7 @@ class NexaSaverHDF5():
 
         # Save attributes
         f[file_level].attrs['Nspatial_clusters'] = nexa_object.Nspatial_clusters
-        f[file_level].attrs['Ntime_clusters'] = nexa_object.Ntime_clusters
+        f[file_level].attrs['Ndata_clusters'] = nexa_object.Ntime_clusters
         f[file_level].attrs['Nembeeding'] = nexa_object.Nembedding
         f[file_level].attrs['Git'] = 'Not implemented yet'
 
@@ -122,99 +122,4 @@ class NexaSaverHDF5():
 
         # Close the file
         f.close()
-
-    def save_SLM_processing(self, nexa_object, run_name='lag_structure'):
-        """
-        This saves the production of the SLM
-        """
-
-        # Things for shorthand access
-        f = self.f
-        a_sensor = nexa_object.sensors.sensors[0]
-        
-        # Time structure
-        root = '/'
-        lag_structure_type = run_name
-        file_level = root + lag_structure_type + '/'
-        
-        # SLM
-        name_to_store = 'SLM'
-        f[file_level + name_to_store] = nexa_object.SLM
-        # Lags
-        name_to_store = 'lags'
-        f[file_level + name_to_store] = a_sensor.lag_structure.window_size
-        # Weights or filters
-        name_to_store = 'weights'
-        f[file_level + name_to_store] = a_sensor.lag_structure.weights
-
-        # Attributes
-        f[file_level].attrs['dt'] = a_sensor.dt
-        f[file_level].attrs['window_size'] = a_sensor.lag_structure.window_size
-        f[file_level].attrs['Nwindow_size'] = nexa_object.sensors.Nwindow_size
-        f[file_level].attrs['Nlags'] = nexa_object.Nlags
-        f[file_level].attrs['format of spatial'] = 'Nspatial_clusters-Ntime_clusters-Nembedding'
-
-
-    def save_nexa_processing(self, nexa_object, run_name='lag_structure'):
-        """
-        This saves the nexa part after the SLM and the temporal part have been
-        saved
-        """
-        # Things for shorthand access
-        f = self.f
-        
-        # Time structure
-        root = '/'
-        lag_structure_type = run_name
-        file_level = root + lag_structure_type + '/'
-
-        # Nexa structure
-        spatial_name = str(nexa_object.Nspatial_clusters) + '-'
-        spatial_name += str(nexa_object.Ntime_clusters) + '-'
-        spatial_name += str(nexa_object.Nembedding)
-
-        file_level += spatial_name + '/'
-
-        name_to_store = 'STDM'
-        f[file_level + name_to_store] = nexa_object.STDM
-
-        # Code vectors
-        name_to_store = '/code vectors'
-        f[file_level + name_to_store] = nexa_object.build_code_vectors()
-
-        name_to_store = '/code vectors_distance'
-        f[file_level + name_to_store] = nexa_object.build_code_vectors_distance()
-
-        name_to_store = '/code vectors_winner'
-        f[file_level + name_to_store] = nexa_object.build_code_vectors_winner()
-
-        name_to_store = '/code vectors_softmax'
-        f[file_level + name_to_store] = nexa_object.build_code_vectors_softmax()
-
-        # Time
-        name_to_store = '/time'
-        f[file_level + name_to_store] = nexa_object.sensors.map_SLM_columns_to_time()
-
-        # Index to cluster
-        name_to_store = '/index_to_cluster'
-        f[file_level + name_to_store] = nexa_object.index_to_cluster
-
-        # Save attributes
-        f[file_level].attrs['Nspatial_clusters'] = nexa_object.Nspatial_clusters
-        f[file_level].attrs['Ntime_clusters'] = nexa_object.Ntime_clusters
-        f[file_level].attrs['Nembeeding'] = nexa_object.Nembedding
-        f[file_level].attrs['Nlags'] = nexa_object.Nlags
-        f[file_level].attrs['Git'] = 'Not implemented yet'
-
-        # Create cluster to index map
-        grp = f.create_group(file_level + 'cluster_to_index')
-
-        # Cluster to index
-        for cluster, indexes in nexa_object.cluster_to_index.items():
-            grp[str(cluster)] = indexes
-
-        # Cluster to time centers
-        grp = f.create_group(file_level + 'cluster_to_time_centers')
-        for cluster, time_centers in nexa_object.cluster_to_time_centers.items():
-            grp[str(cluster)] = time_centers
 
